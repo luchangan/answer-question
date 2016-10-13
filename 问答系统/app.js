a8be789a707a72faca7a1f6e4050028a5feb0c84
卢鹏程 <1120336291@qq.com>
2016-10-13 14:47:09 +0800

    formdata上传图片

--- 问答系统/app.js
+++ 问答系统/app.js
@@ -7,18 +7,32 @@ const multer = require('multer');
 const fs = require('fs');
 const app = express();
 const form = multer();
+const cookieParser = require("cookie-parser");
+app.use(cookieParser())
 app.use(express.static('www'));
 
+// 上传图片
 var storage = multer.diskStorage({
-    // cb 回调函数 callback
     destination: function (req, file, cb) {
         cb(null, 'uploads')
     },
     filename: function (req, file, cb) {
+        // 在req加入一个属性file，等于一个file对象
+        req.file = file;
+        var arr = file.originalname.lastIndexOf('.');
+        // slice() 可从已有的数组中返回选定的元素规定从何处开始选取
+        var str1 = file.originalname.slice(arr);
+        console.log(arr);
+        var name = req.cookies.user;
+        console.log(req.cookies.user);
         // file原始上传图片相关信息的对象
         console.log(file)
-        // cb 第二个参数：上传到服务器上的名字
-        cb(null, file.originalname)
+        if(file.mimetype=='image/jpeg'){
+            cb(null, name+str1);
+        }else{
+            console.log('不正确');
+        }
+
     }
 })
 
@@ -33,9 +47,25 @@ app.get('/', function(req, res){
 // 注册页面
 app.post('/jqpost', function (req, res) {
     console.log('服务器连通');
-    req.body.ip =req.ip;
-    req.body.time = new Date().getTime();
-    var user = req.body;
+    function formatIp(ip) {
+        ip=ip=="::1"?"192.168.0.1":req.ip;
+        if((ip).startsWith('::ffff:')){
+            ip = ip.substring(7);
+            return ip;
+        }
+    }
+    req.body.ip =formatIp(req.ip);
+    console.log(req.body.ip);
+    var date = new Date()
+    req.body.time = date.toLocaleDateString()+" " + date.toLocaleTimeString();
+    var user = {};
+    user.name = req.body.name;
+    user.password = req.body.password;
+    user.sex = req.body.sex;
+    user.email = req.body.email;
+    user.course = req.body.course;
+    user.ip = req.body.ip;
+    user.time = req.body.time;
     var userStr = JSON.stringify(user);
     fs.readFile('user.txt', function (err, data) {
         var usersStr = data.toString().trim();
@@ -83,41 +113,65 @@ app.post('/Jqindex/post', function (req, res) {
 
 // 主页读取问题
 app.post('/Jqindex/ask', function (req, res) {
-    fs.readFile('messages.txt', function (err, data) {
-        var ask = data.toString().trim();
-        var askStr = JSON.parse('[' + ask + ']')
-        console.log(askStr);
-        res.status(200).send(askStr)
+//    fs.readFile('messages.txt', function (err, data) {
+//        var ask = data.toString().trim();
+//        var askStr = JSON.parse('[' + ask + ']')
+//        console.log(askStr);
+//        res.status(200).send(askStr)
+//    })
+    fs.readdir('questions',function(err,files){
+        console.log('问答文件夹文件：'+files);
+        var arr = (files.toString().trim()).split(',');
+        console.log(arr);
+        for(var i in arr){
+            console.log('questions/'+arr[i]);
+            fs.readFile('questions/'+arr[i], function (err, data) {
+                var str = data.toString().trim();
+                console.log('这是文件'+str);
+            })
+        }
     })
 })
 
 // 提交问题
 app.post('/Jqask', function (req,res) {
-    req.body.time = new Date().getTime();
+    var date = new Date()
+    req.body.time = date.toLocaleDateString()+" " + date.toLocaleTimeString();
     var ask = req.body.ask;
     var reg1 = /</mg;
     var reg2 = />/mg;
     ask = ask.replace(reg1,'&lt;');
     ask = ask.replace(reg2,'&gt;');
     var user = req.body;
-//    console.log(user);
+    console.log(user);
     var userStr = JSON.stringify(user);
-    fs.readFile('messages.txt', function (err,data) {
-        var ask = data.toString().trim();
-        var askStr = JSON.parse('[' + ask + ']')
-        var douhao = ask.length > 0 ? ',' : '';
-
-        fs.appendFile('messages.txt',douhao+userStr, function (err) {
+        fs.appendFile('questions/'+date.getTime()+'.txt',userStr, function (err) {
             res.status(200).send('aaa');
+            console.log('成功');
         })
-    })
 })
 
-
+// 提交回答
+app.post('/Jqanswer', function (req, res) {
+    var date = new Date();
+    req.body.time = date.toLocaleDateString()+' '+date.toLocaleTimeString();
+    var answer = req.body.answer;
+    var reg1 = /</mg;
+    var reg2 = />/mg;
+    answer = answer.replace(reg1,'&lt;');
+    answer = answer.replace(reg2,'&gt;');
+    var user = req.body;
+    var userStr = JSON.stringify(user);
+//    console.log(userStr);
+    res.status(200).send('nihao')
+    var usersStr = JSON.parse('['+userStr+']');
+    console.log(usersStr)
+})
 
 // 上传图片
 app.post('/JqPhoto', upload.single('photo'), function (req, res) {
-//    console.log(req.body);
+    console.log('成功')
+    console.log(req.body);
     res.status(200).send('成功')
 })
 
